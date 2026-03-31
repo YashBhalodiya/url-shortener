14d4489a7efb7e534730256007ed703dd758a160
YashBhalodiya <yashbhalodiya650@gmail.com>
2026-04-01 02:25:48 +0530

    feat: implement URL generation and retrieval functions

---
+++ controller/url.controller.js
@@ -0,0 +1,40 @@
+const shortid = require("shortid");
+const { URL } = require("../model/url.model");
+
+async function handleGenerateNewShortURL(req, res) {
+  const body = req.body;
+  console.log(body);
+
+  if (!body.url) {
+    return res.status(400).json({ err: "url is required" });
+  }
+  const generatedShortId = shortid();
+  await URL.create({
+    shortId: generatedShortId,
+    redirectURL: body.url,
+    visitorHistory: [],
+  });
+  return res.json({ id: generatedShortId });
+}
+
+async function handleGetNewShortURL(req, res) {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visiterHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    },
+  );
+  return res.redirect(entry.redirectURL);
+}
+
+module.exports = {
+  handleGenerateNewShortURL,
+  handleGetNewShortURL,
+};
