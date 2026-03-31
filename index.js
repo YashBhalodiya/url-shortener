875da00efe537d61a62cc037f87e98163825d3d4
YashBhalodiya <yashbhalodiya650@gmail.com>
2026-04-01 02:25:34 +0530

    feat: add Express server setup and MongoDB connection

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const connectDb = require('./config/db')
+const urlRouter = require('./routes/url.route')
+
+const app = express()
+
+// Middleware
+app.use(express.json())
+
+// DB Connection
+connectDb("mongodb://localhost:27017/short-url")
+.then(() => console.log("Connect to MongoDB"))
+.catch((err) => console.log("err", err))
+
+// Route
+app.use('/url', urlRouter)
+
+
+app.listen(8001, () => console.log("Server is running on 8001"))
