254366c913aaf392f2d7d7d11a9d4f4ae3a62f4e
YashBhalodiya <yashbhalodiya650@gmail.com>
2026-05-16 18:39:32 +0530

    feat: initialize full-stack URL shortener with Docker containerization and CI/CD pipeline

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDb = require('./config/db')
+const cors = require('cors')
 const urlRouter = require('./routes/url.route')
 const { handleGetNewShortURL } = require('./controller/url.controller')
 require('dotenv').config()
@@ -8,12 +9,13 @@ const app = express()
 
 // Middleware
 app.use(express.json())
+app.use(cors())
 
 // DB Connection
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/short-url"
+const MONGO_URI = process.env.MONGO_URI || "mongodb://mongo:27017/short-url" // if have connetion string in env, if not then the local will get connected
 connectDb(MONGO_URI)
-.then(() => console.log("Connect to MongoDB"))
-.catch((err) => console.log("err", err))
+  .then(() => console.log("Connect to MongoDB"))
+  .catch((err) => console.log("err", err))
 
 // Route
 app.use('/url', urlRouter)
@@ -22,8 +24,8 @@ app.use('/url', urlRouter)
 app.get('/:shortId', handleGetNewShortURL)
 
 const PORT = process.env.PORT || 8001;
-if(process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, '0.0.0.0' , () => console.log(`Server is running on ${PORT}`))
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, '0.0.0.0', () => console.log(`Server is running on ${PORT}`))
 }
 
 module.exports = app;
